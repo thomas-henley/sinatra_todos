386b019604284f95ee06ca330856975eb61b04c0
Thomas Henley <t4.crow@gmail.com>
2022-03-12 04:18:25 +0000

    add CRUD functionality for list objects

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
 
 configure do
@@ -24,7 +25,55 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
-post "/lists" do
-  session[:lists] << {name: params[:list_name], todos: []}
+get "/lists/:index" do
+  @list = session[:lists][params["index"].to_i]
+  erb :list, layout: :layout
+end
+
+get "/lists/:index/edit" do
+  @list = session[:lists][params["index"].to_i]
+  erb :edit_list, layout: :layout
+end
+
+post "/lists/:index" do
+  list_name = params[:list_name].strip
+  @list = session[:lists][params["index"].to_i]
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been edited."
+    redirect "/lists/#{params[:index]}"
+  end
+end
+
+post "/lists/:index/delete" do
+  session[:lists].delete_at(params[:index].to_i)
+  session[:success] = "The list has been deleted."
   redirect "/lists"
+end
+
+# Return an error message if the name is invalid, else return nil
+def error_for_list_name(name)
+  if session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  elsif !(1..100).cover?(name.size)
+    "List name must be between 1 and 100 characters."
+  end
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
 end
