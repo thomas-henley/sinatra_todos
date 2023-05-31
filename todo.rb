088b975708e926b76d621cd8e5862edf42307b7a
Thomas Henley <t4.crow@gmail.com>
2023-05-31 18:10:07 -0400

    initial commit

---
+++ todo.rb
@@ -0,0 +1,11 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+get "/" do
+  @lists = [
+    {name: "Lunch Groceries"},
+    {name: "Dinner Groceries"}
+  ]
+  erb :lists, layout: :layout
+end
